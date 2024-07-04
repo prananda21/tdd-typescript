13197a782ae806e6a4c47a6d395e67197fce8fe4
Prananda Yoga <pranandayoga21@gmail.com>
2024-07-04 16:04:06 +0800

    test: add unit test for getName (full coverage)

--- test/unit-testing.test.ts
+++ test/unit-testing.test.ts
@@ -1,7 +1,11 @@
-import { sumNumber } from "../src/number";
+import { getName, sumNumber } from "../src/number";
 
 describe("Test file test.ts", () => {
 	it("should have value of sum function", () => {
 		expect(sumNumber(1, 1)).toEqual(2);
+	});
+
+	it("should ", () => {
+		expect(getName("Yoga")).toBe("Hello Yoga");
 	});
 });
