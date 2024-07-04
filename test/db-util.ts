044305d6da04b2582b829c52b946842c2bb224a7
Prananda Yogathama Pribadi <pranandayoga21@Pranandas-MacBook-Air.local>
2024-07-04 13:21:03 +0800

    docs: add docs comment on test/db-util.ts

--- test/db-util.ts
+++ test/db-util.ts
@@ -1,6 +1,10 @@
 import { User } from "@prisma/client";
 import { prismaClient } from "../src/app/database";
 
+/**
+ * This function is responsible for deleting all data users from database
+ */
+
 export class UserTest {
 	static async delete() {
 		await prismaClient.user.deleteMany({
